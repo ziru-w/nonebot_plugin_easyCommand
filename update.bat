ae02d9e5376bc3c88c5af51e74f46b78acd38105
ziru-w <77319678+ziru-w@users.noreply.github.com>
2022-07-14 18:06:59 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
-git checkout --orphan temp
+git checkout --orphan latest_branch
 git add -A
-git commit -am
-git branch -D main
-git branch -m main
-git push -f origin main
+git commit -am "清除所有历史版本以减少仓库大小，请重新从远程拷贝此仓库"
+git branch -D master
+git branch -m master
+git push -f origin master
+
